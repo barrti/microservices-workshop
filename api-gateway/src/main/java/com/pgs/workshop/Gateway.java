8ae41304873d129a2ab06e552ff8b50f16d0a97c
Bartosz Miedlar <bmiedlar@pgs-soft.com>
2018-05-17 10:37:56 +0200

    Part 1 - Create API Gateway

---
+++ api-gateway/src/main/java/com/pgs/workshop/Gateway.java
@@ -0,0 +1,14 @@
+package com.pgs.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Gateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class, args);
+    }
+}
