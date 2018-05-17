42b479259d6946ed5f8a48616bac1a3b26137f39
Bartosz Miedlar <bmiedlar@pgs-soft.com>
2018-05-17 11:29:43 +0200

    Part 2 - Add pre filter

--- api-gateway/src/main/java/com/pgs/workshop/Gateway.java
+++ api-gateway/src/main/java/com/pgs/workshop/Gateway.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -14,4 +15,11 @@ public class Gateway {
     public static void main(String[] args) {
         SpringApplication.run(Gateway.class, args);
     }
+
+    @Bean
+    public ZuulFilter headerFilter() {
+        return new HeaderFilter();
+    }
 }
+
+
